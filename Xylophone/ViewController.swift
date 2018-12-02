de5676bd0111ffaa067fcc9ee38bbb93e096bd03
Donlaya <Tonan@NanMacBook.local>
2018-12-02 13:38:16 +0700

    Success

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
+import ChameleonFramework
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    var sound : AVAudioPlayer!
+    var mySound = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var userChose = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setImage()
+        
+        setBackground()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func keyButton(_ sender: UIButton) {
+        
+        userChose = mySound[sender.tag]
+        
+        xylophone()
     }
-
-
+    
+    func xylophone() {
+        
+        let soundURL = Bundle.main.url(forResource: userChose, withExtension: "wav")
+        do {
+            try sound = AVAudioPlayer(contentsOf: soundURL!)
+        } catch  {
+            print(error)
+        }
+        sound.play()
+    }
+    
+    func setImage() {
+        
+        self.logoImage.image = UIImage(named: "logo")
+    }
+    
+    func setBackground() {
+        
+        view.backgroundColor = UIColor.randomFlat()
+    }
+    
 }
 
